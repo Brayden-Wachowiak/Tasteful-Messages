0a35a444fb25935e28b3b86ef497f6a81f909f2c
Brayden-Wachowiak <bjwachowiak@avaya.com>
2024-05-17 12:40:14 -0500

    Changed Layout

---
+++ src/components/header/sections/index.ts
@@ -0,0 +1,8 @@
+import Navigation from "./Navigation.astro";
+export { Navigation };
+
+import ResumeButton from "./ResumeButton.astro";
+export { ResumeButton };
+
+import NavigationButton from "./NavigationButton.astro";
+export { NavigationButton };
---
+++ src/pages/home/index.ts
@@ -0,0 +1,5 @@
+import HomeLayout from "./HomeLayout.astro";
+export { HomeLayout };
+
+import HomeCard from "./HomeCard.astro";
+export { HomeCard };
